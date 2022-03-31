6585fdd34c9e43df09eefb8d5955d41d09bc4ce5
ProBroStudio <81253017+ProBroStudio@users.noreply.github.com>
2022-03-31 15:42:13 +0300

    Update Site-HTML5-Shipping.UE4.js

--- Site-HTML5-Shipping.UE4.js
+++ Site-HTML5-Shipping.UE4.js
@@ -1155,7 +1155,7 @@ $(document).ready(function() {
 		// ----------------------------------------
 		// MORE JS
 		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/ProBroStudio/siteTest1/b02583f95b380de6d6d5846dab243f76689a404d/Site-HTML5-Shipping.data.js'));
-		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
+		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/ProBroStudio/siteTest1/899fd87877d947666984e979d144e7e6da59a028/Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
 			fetchOrDownloadAndStore(db, Module.locateFile('Site-HTML5-Shipping.data')).then(function(dataBlob) {
